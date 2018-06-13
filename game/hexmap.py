6dade9df4f891529e3a06916ccf1b6c80d0b5615
Zsombor <prisznyak.zsombor@gmail.com>
2018-06-14 01:43:48 +0200

    - TODO hexmap

--- game/hexmap.py
+++ game/hexmap.py
@@ -1,5 +1,12 @@
 class Hex:
-    pass
+    def __init__(self, x: int, y: int):
+        """ Are you surprised? """
+        self.x = x
+        self.y = y
 
 class HexMap:
-    pass
+    def __init__(self, sizex: int, sizey: int = None):
+        self.map = #todo
+        for x in range(sizex):
+            for y in range(sizey):
+                self.map[x][y] = Hex(x, y)
