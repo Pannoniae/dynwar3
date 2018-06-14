51a2d373845aacea953d514807482e15e670e949
Zsombor <prisznyak.zsombor@gmail.com>
2018-06-14 22:16:09 +0200

    - hex addition, hex direction

--- game/app.py
+++ game/app.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy
 
 import cairo
@@ -7,6 +9,18 @@ from game.util import bgra_surf_to_rgba_string
 
 width, height = 800, 600
 
+def draw(ctx):
+    ctx.set_line_width(15)
+    ctx.arc(320, 240, 200, 0, 2 * math.pi)
+
+    #                   r    g  b    a
+    ctx.set_source_rgba(0.6, 0, 0.4, 1)
+    ctx.fill_preserve()
+
+    #                   r  g     b    a
+    ctx.set_source_rgba(0, 0.84, 0.2, 0.75)
+    ctx.stroke()
+
 pygame.display.init()
 screen = pygame.display.set_mode((width, height), 0, 32)
 
@@ -14,6 +28,7 @@ data = numpy.empty(width * height * 4, dtype = numpy.int8)
 cairo_surface = cairo.ImageSurface.create_for_data(
     data, cairo.FORMAT_ARGB32, width, height, width * 4)
 ctx = cairo.Context(cairo_surface)
+draw(ctx)
 data_string = bgra_surf_to_rgba_string(cairo_surface)
 
 # Create PyGame surface
--- game/hexmap.py
+++ game/hexmap.py
@@ -21,19 +21,33 @@ class TerrainType:
 
 
 class Hex:
-    def __init__(self, x: int, y: int, terrain):
+    def __init__(self, x: int, y: int, terrain = TerrainType.t_clr):
         """ Are you surprised? """
         self.x = x
         self.y = y
         self.terrain = terrain
 
+    def __add__(self, other):
+        return Hex(self.x + other.x, self.y + other.y)
+
+
+
+class Direction:
+    SOUTHEAST = 0
+    NORTHEAST = 1
+    NORTH = 2
+    NORTHWEST = 3
+    SOUTHWEST = 4
+    SOUTH = 5
+
+
 class HexMap:
-    neighbors = [(),
-                 (),
-                 (),
-                 (),
-                 (),
-                 ()]
+    neighbors = [Hex(+1, 0),
+                 Hex(+1, -1),
+                 Hex(0, -1),
+                 Hex(-1, 0),
+                 Hex(-1, +1),
+                 Hex(0, +1)]
 
     def __init__(self, sizex: int, sizey: int = None):
         if not sizey:
@@ -41,6 +55,9 @@ class HexMap:
         self.map = [[Hex(x, y, TerrainType.t_clr) for x in range(sizex)] for y in range(sizey)]
         self.sizex = sizex
         self.sizey = sizey
+
+    def get_neighbor(self, hex: Hex, direction):
+        return Hex + self.neighbors[direction]
 
     def __str__(self):
         return f'<HexMap({self.sizex}, {self.sizey})>'
