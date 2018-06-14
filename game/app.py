a87856d80da7d9b6c698af9bbe3a13f19c81ae23
Zsombor <prisznyak.zsombor@gmail.com>
2018-06-14 16:33:54 +0200

    - setting up PyCairo

--- game/app.py
+++ game/app.py
@@ -1,12 +1,64 @@
-import sys
+import numpy
 
+import cairo
 import pygame
-pygame.init()
+import math
 
-w, h = 800, 600
-screen = pygame.display.set_mode((w, h))
+from game.example import bgra_surf_to_rgba_string
 
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
+def draw(ctx):
+    ctx.set_line_width(15)
+    ctx.arc(320, 240, 200, 0, 2 * math.pi)
+
+    #                   r    g  b    a
+    ctx.set_source_rgba(0.6, 0, 0.4, 1)
+    ctx.fill_preserve()
+
+    #                   r  g     b    a
+    ctx.set_source_rgba(0, 0.84, 0.2, 0.5)
+    ctx.stroke()
+
+width, height = 800, 600
+
+pygame.display.init()
+screen = pygame.display.set_mode((width, height), 0, 32)
+
+# Create raw surface data (could also be done with something else than
+# NumPy)
+data = numpy.empty(width * height * 4, dtype=numpy.int8)
+
+# Create Cairo surface
+cairo_surface = cairo.ImageSurface.create_for_data(
+    data, cairo.FORMAT_ARGB32, width, height, width * 4)
+
+# Draw with Cairo on the surface
+ctx = cairo.Context(cairo_surface)
+draw(ctx)
+ # On little-endian machines (and perhaps big-endian, who knows?),
+# Cairo's ARGB format becomes a BGRA format. PyGame does not accept
+# BGRA, but it does accept RGBA, which is why we have to convert the
+# surface data. You can check what endian-type you have by printing
+# out sys.byteorder
+data_string = bgra_surf_to_rgba_string(cairo_surface)
+
+# Create PyGame surface
+pygame_surface = pygame.image.frombuffer(
+    data_string, (width, height), 'RGBA')
+
+# Draw a white circle to the screen using pygame.
+radius = int(min((width, height))/2*0.7)
+pos = [int(a/2) for a in (width, height)]
+pygame.draw.circle(screen, (255,255,255), pos, radius)
+
+# Draw a smaller black circle to the screen using Cairo.
+context = cairo.Context(cairo_surface)
+context.set_source_rgb(0, 0, 0)
+context.arc(width/2, height/2, min((width, height))/2*0.5, 0, 2*math.pi)
+context.fill()
+
+screen.blit(pygame_surface, (0,0))
+pygame.display.flip()
+
+clock = pygame.time.Clock()
+while not pygame.QUIT in [e.type for e in pygame.event.get()]:
+    clock.tick(30)
