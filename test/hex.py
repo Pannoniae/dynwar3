9dae651c9a8ab09727c3fef2babfec2b4718c923
Zsombor <prisznyak.zsombor@gmail.com>
2018-06-14 22:23:24 +0200

    - fixed tests

--- test/hex.py
+++ test/hex.py
@@ -6,7 +6,7 @@ def test_terrain_set():
     assert h.terrain == TerrainType.t_cty
 
 def test_major_terrain():
-    assert TerrainType.majorType(TerrainType.t_hll) == TerrainType.t_mtn
+    assert TerrainType.major_type_for(TerrainType.t_hll) == TerrainType.t_mtn
 
 def test_minor_terrain():
-    assert TerrainType.isMinorType(TerrainType.t_hll)
+    assert TerrainType.is_minor_type(TerrainType.t_hll)
