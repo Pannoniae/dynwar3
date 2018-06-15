3978327b2ed9a955481b782655d0e9f275a4eb8b
Zsombor <prisznyak.zsombor@gmail.com>
2018-06-15 02:35:54 +0200

    - created highlighting

--- game/app.py
+++ game/app.py
@@ -1,4 +1,5 @@
 import math
+import sys
 
 import numpy
 
@@ -11,12 +12,12 @@ from game.util import bgra_surf_to_rgba_string
 
 width, height = 800, 600
 
-def draw(ctx):
+def draw(ctx, mouse_pos):
     ctx.set_line_width(1)
     ctx.set_source_rgba(0.6, 0, 0.4, 1)
     for hex in hm:
         r, g, b = layout.colors[hex.terrain]
-        ctx.set_source_rgba(r, g, b, 1)
+        ctx.set_source_rgba(r, g, b, 0.5)
         for corner in range(0, 7):
             x, y = flat_hex_corner(Hex(*layout.get_hex_position(hex)), layout.size, corner)
             ctx.line_to(x, y)
@@ -25,6 +26,13 @@ def draw(ctx):
         for corner in range(0, 7):
             x, y = flat_hex_corner(Hex(*layout.get_hex_position(hex)), layout.size, corner)
             ctx.line_to(x, y)
+        ctx.stroke()
+
+    if mouse_pos:
+        #print(layout.get_containing_hex_center(mouse_pos), layout.pixel_to_hex(mouse_pos))
+        ctx.set_source_rgba(1, 0, 0, 1)
+        #ctx.move_to(*layout.get_containing_hex_center(mouse_pos))
+        ctx.arc(*layout.get_containing_hex_center(mouse_pos), 10, 0, math.pi * 2)
         ctx.stroke()
 
 
@@ -40,16 +48,19 @@ i.terrain = TerrainType.t_hll
 hm.set_hex((2, 2), i)
 layout = HexMapLayout(hm, 20, (100, 100))
 ctx = cairo.Context(cairo_surface)
-draw(ctx)
-data_string = bgra_surf_to_rgba_string(cairo_surface)
-
-# Create PyGame surface
-pygame_surface = pygame.image.frombuffer(
-    data_string, (width, height), 'RGBA')
-
-screen.blit(pygame_surface, (0,0))
-pygame.display.update()
 
 clock = pygame.time.Clock()
-while not pygame.QUIT in [e.type for e in pygame.event.get()]:
-    clock.tick(30)
+while 1:
+    for e in pygame.event.get():
+        if e.type == pygame.MOUSEMOTION:
+            pos = pygame.mouse.get_pos()
+            draw(ctx, pos)
+        elif e.type == pygame.QUIT:
+            sys.exit()
+        data_string = bgra_surf_to_rgba_string(cairo_surface)
+
+        # Create PyGame surface
+        pygame_surface = pygame.image.frombuffer(
+                data_string, (width, height), 'RGBA')
+        screen.blit(pygame_surface, (0, 0))
+        pygame.display.update()
--- game/layout.py
+++ game/layout.py
@@ -9,7 +9,7 @@ class HexMapLayout:
     colors = {TerrainType.t_clr: (0.5, 0.3, 0.1),
               TerrainType.t_hll: (0.7, 0.2, 0.3)}
 
-    def __init__(self, hexmap: HexMap, size = 20, offset = (0, 0)):
+    def __init__(self, hexmap: HexMap, size: int = 20, offset: tuple = (0, 0)):
         self.size = size
         self.offset = offset
 
@@ -19,13 +19,53 @@ class HexMapLayout:
         x += self.offset[0]
         y += self.offset[1]
         return x, y
-        #pos1 = 0
-        #x = self.size * 1.5
-        #for i in range(hex.x):
-        #    pos1 += x
-        #pos2 = hex.y * (math.sqrt(3) * self.size)
-        #for i in range(hex.x):
-        #    pos2 += (math.sqrt(3) * self.size) / 2
-        #pos1 += self.offset[0]
-        #pos2 += self.offset[1]
-        #return pos1, pos2
+
+    def pixel_to_hex(self, position: tuple):
+        _position = list(position)
+        _position[0] -= self.offset[0]
+        _position[1] -= self.offset[1]
+        x = (2. / 3 * _position[0]) / self.size
+        y = (-1. / 3 * _position[0] + math.sqrt(3) / 3 * _position[1]) / self.size
+        return self.hex_round((x, y))
+
+    def hex_round(self, position: tuple):
+        """ USE THIS, otherwise results WILL be inexact """
+        hex = _axial_to_cube(position)
+        cube = _cube_round(hex)
+        hex2 = _cube_to_axial(cube)
+        return Hex(*hex2)
+
+
+    def get_containing_hex_center(self, position: tuple):
+        hex = self.pixel_to_hex(position)
+        print(self.get_hex_position(hex))
+        return self.get_hex_position(hex)
+
+def _cube_round(cube):
+    rx = round(cube[0])
+    ry = round(cube[1])
+    rz = round(cube[2])
+
+    x_diff = abs(rx - cube[0])
+    y_diff = abs(ry - cube[1])
+    z_diff = abs(rz - cube[2])
+
+    if x_diff > y_diff and x_diff > z_diff:
+        rx = -ry-rz
+    elif y_diff > z_diff:
+        ry = -rx-rz
+    else:
+        rz = -rx-ry
+
+    return rx, ry, rz
+
+def _cube_to_axial(cube: tuple):
+    x = cube[0]
+    y = cube[2]
+    return x, y
+
+def _axial_to_cube(hex: tuple):
+    x = hex[0]
+    z = hex[1]
+    y = -x-z
+    return x, y, z
