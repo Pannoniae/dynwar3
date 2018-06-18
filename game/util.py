b7513ff544a971effa8d43cf33c8896f55070f5a
Zsombor <prisznyak.zsombor@gmail.com>
2018-06-18 14:47:06 +0200

    - cleaned code around ctypes, removed useless code

--- game/util.py
+++ game/util.py
@@ -1,16 +1,9 @@
 import ctypes
 
 import cairo
-from PIL import Image
 
-
-def bgra_surf_to_rgba_string(cairo_surface):
-    # We use PIL to do this
-    img = Image.frombuffer(
-        'RGBA', (cairo_surface.get_width(),
-                 cairo_surface.get_height()),
-        cairo_surface.get_data().tobytes(), 'raw', 'BGRA', 0, 1)
-    return img.tobytes('raw', 'RGBA', 0, 1)
+PyBUF_READ = 0x100
+PyBUF_WRITE = 0x200
 
 
 def get_cairo_surface(pygame_surface):
@@ -41,10 +34,9 @@ def get_cairo_surface(pygame_surface):
 
     surface = Surface.from_address(id(pygame_surface))
     ss = SDL_Surface.from_address(surface.SDL_Surface)
-    #pixels_ptr = ctypes.pythonapi.PyBuffer_FromReadWriteMemory(ctypes.c_void_p(ss.pixels), ss.pitch * ss.h)
     pixels_ptr = ctypes.pythonapi.PyMemoryView_FromMemory(ctypes.c_void_p(ss.pixels),
                                                           ss.pitch * ss.h,
-                                                          0x200)
+                                                          PyBUF_WRITE)
     pixels = ctypes.cast(pixels_ptr, ctypes.py_object).value
     print(pixels.readonly)
     return cairo.ImageSurface.create_for_data(pixels, cairo.FORMAT_RGB24, ss.w, ss.h, ss.pitch)
