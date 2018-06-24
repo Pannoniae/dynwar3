29aa824882217a2322c5155f91451d2fbdfce198
Zsombor <prisznyak.zsombor@gmail.com>
2018-06-24 22:06:38 +0200

    - refactored rendering

--- game/io/renderer.py
+++ game/io/renderer.py
@@ -1,3 +1,4 @@
+import logging
 import math
 from typing import List
 
@@ -8,49 +9,93 @@ from game.layout import HexMapLayout
 
 
 class Renderer:
-    colors = {
-        'edge': (0.0, 0.5, 1.0),
-        'HP': (0.9, 0.3, 0.0),
-        TerrainType.t_clr: (0.5, 0.3, 0.1),
-        TerrainType.t_hll: (0.7, 0.2, 0.3)}
+
 
     def __init__(self, screen: pygame.Surface, game):
+
+        self.drawing = {
+            'edge': (0.0, 0.5, 1.0),
+            'HP': (0.9, 0.3, 0.0),
+            TerrainType.t_clr: (0.5, 0.3, 0.1),
+            TerrainType.t_hll: self.draw_mountain # (0.7, 0.2, 0.3)}
+        }
+
         self.ctx = cairo.Context(game.surf)
         self.screen = screen
         self.game = game
         self.clock = pygame.time.Clock()
         self.layout = HexMapLayout(32, (100, 100))
+
+    def hex_outline(self, hex: Hex):
+        for corner in range(0, 7):
+            x, y = self.layout.flat_hex_corner(self.layout.get_hex_position(hex.pos), self.layout.size, corner)
+            self.ctx.line_to(x, y)
+
+    def draw_mountain(self, x: int, y: int):
+        self.ctx.save()
+
+        self.ctx.set_source_rgba(0.5, 0.5, 0.5, 1)
+        self.ctx.set_line_width(3)
+        size = self.layout.size * 2
+        self.ctx.move_to(x + size / 4, y + size / 2)
+        self.ctx.line_to(x + size * 3 / 8, y + size / 3)
+        self.ctx.line_to(x + size / 2, y + size / 2)
+        self.ctx.line_to(x + size * 5 / 8, y + size / 3)
+        self.ctx.line_to(x + size * 3 / 4, y + size / 2)
+        self.ctx.stroke()
+
+        self.ctx.restore()
+
+    def draw_hex(self, hex: Hex):
+        self.hex_outline(hex)
+        self.ctx.fill()
+
     def draw(self, mouse_pos):
 
         self.ctx.set_source_rgba(0, 0, 0, 1)
         self.ctx.paint()
 
         self.ctx.set_line_width(1)
-        self.ctx.set_source_rgba(0.6, 0, 0.4, 1)
+
         units: List[Hex] = []
+
         for hex in self.game.hexmap:
-            r, g, b = self.colors[hex.terrain]
-            self.ctx.set_source_rgba(r, g, b, 1)
-            for corner in range(0, 7):
-                x, y = self.layout.flat_hex_corner(self.layout.get_hex_position(hex.pos), self.layout.size, corner)
-                self.ctx.line_to(x, y)
-            self.ctx.fill_preserve()
+
+            val = self.drawing[hex.terrain]
+            if callable(val):
+                r, g, b = self.drawing[TerrainType.t_clr]
+                self.ctx.set_source_rgba(r, g, b, 1)
+                self.draw_hex(hex)
+                val(*self.layout.get_hex_upper_corner(hex.pos))
+
+            elif isinstance(val, tuple):
+                r, g, b = val
+                self.ctx.set_source_rgba(r, g, b, 1)
+                self.draw_hex(hex)
+
+            else:
+                logging.warning(f'No handling for drawer {val}! Drawing nothing.')
+                self.ctx.set_source_rgba(1, 1, 1, 1)
+                self.draw_hex(hex)
+
+
+            self.hex_outline(hex)
+
             if hex.country == 0:
                 self.ctx.set_source_rgba(0.0, 0.0, 1.0, 0.2)
             elif hex.country == 1:
                 self.ctx.set_source_rgba(1.0, 0.0, 0.0, 0.2)
-            else:
-                print('Error')
             self.ctx.fill()
+
             #else:
             #    self.ctx.set_source_surface(self.game.surf.create_from_png(f'data/terrain/grass.png'),
             #                                *self.layout.get_hex_upper_corner(hex.pos))
             #    self.ctx.paint()
-            if self.game.debug:
-                self.ctx.set_source_rgba(*self.colors['edge'], 1)
-                for corner in range(0, 7):
-                    x, y = self.layout.flat_hex_corner(self.layout.get_hex_position(hex.pos), self.layout.size, corner)
-                    self.ctx.line_to(x, y)
+
+            self.ctx.set_source_rgba(*self.drawing['edge'], 1)
+            for corner in range(0, 7):
+                x, y = self.layout.flat_hex_corner(self.layout.get_hex_position(hex.pos), self.layout.size, corner)
+                self.ctx.line_to(x, y)
             if hex.has_unit():
                 units.append(hex)
             self.ctx.stroke()
@@ -60,7 +105,7 @@ class Renderer:
                                         *self.layout.get_hex_upper_corner(hex.pos))
             self.ctx.paint()
             self.ctx.move_to(*self.layout.get_hex_position(hex.pos))
-            self.ctx.set_source_rgba(*self.colors['HP'], 1)
+            self.ctx.set_source_rgba(*self.drawing['HP'], 1)
             self.ctx.set_line_width(5)
             self.ctx.set_line_cap(cairo.LINE_CAP_ROUND)
             self.ctx.rel_move_to(-(self.layout.size / 2), self.layout.size / 2)
@@ -71,7 +116,7 @@ class Renderer:
         if self.game.debug:
             for widget in self.game.widgets:
                 self.ctx.move_to(*widget.box.topleft)
-                self.ctx.set_source_rgba(*self.colors['edge'], 1)
+                self.ctx.set_source_rgba(*self.drawing['edge'], 1)
                 for pos in ("topleft", "topright", "bottomright", "bottomleft"):
                     self.ctx.line_to(*getattr(widget.box, pos))
                 self.ctx.close_path()
